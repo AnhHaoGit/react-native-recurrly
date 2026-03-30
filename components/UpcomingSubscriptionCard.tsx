7730f374556033061bfc5e8b89b6dd21e545ba05
AnhHaoGit <nguyenanhhao090708@gmail.com>
2026-03-30 21:14:33 +0700

    implement layout fixes

--- components/UpcomingSubscriptionCard.tsx
+++ components/UpcomingSubscriptionCard.tsx
@@ -11,15 +11,19 @@ const UpcomingSubscriptionCard = ({
 }: UpcomingSubscription) => {
   return (
     <View className="upcoming-card">
-      <Image source={icon} className="upcoming-icon" />
-      <View>
-        <Text className="upcoming-price">
-          {formatCurrency(price, currency)}
-        </Text>
-        <Text className="upcoming-meta" numberOfLines={1}>
-          {daysLeft > 1 ? `${daysLeft} days left` : "Last day"}
-        </Text>
+      <View className="upcoming-row">
+        <Image source={icon} className="upcoming-icon" />
+
+        <View>
+          <Text className="upcoming-price">
+            {formatCurrency(price, currency)}
+          </Text>
+          <Text className="upcoming-meta" numberOfLines={1}>
+            {daysLeft > 1 ? `${daysLeft} days left` : "Last day"}
+          </Text>
+        </View>
       </View>
+
       <Text className="upcoming-name" numberOfLines={1}>
         {name}
       </Text>
